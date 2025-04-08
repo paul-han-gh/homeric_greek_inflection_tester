0712172882fe37f78a36f7d4c490a85026dc06ec
Paul Han <paulh@dtric.com>
2025-04-08 10:39:40 -1000

    Add remaining enums for all numbers

--- homeric_greek/declensions/first.py
+++ homeric_greek/declensions/first.py
@@ -6,3 +6,25 @@ class MasculineSingular(StrEnum):
     DATIVE = 'ι'
     ACCUSATIVE = 'ν'
     VOCATIVE = ''
+
+class FeminineSingular(StrEnum):
+    NOMINATIVE = ''
+    GENITIVE = 'ης'
+    DATIVE = 'ι'
+    ACCUSATIVE = 'ν'
+    VOCATIVE = ''
+
+# Both masculine and feminine endings are the same for dual and plural
+class Dual(StrEnum):
+    NOMINATIVE = ''
+    GENITIVE = 'ιιν'
+    DATIVE = 'ιιν'
+    ACCUSATIVE = ''
+    VOCATIVE = ''
+
+class Plural(StrEnum):
+    NOMINATIVE = 'ι'
+    GENITIVE = 'ων'
+    DATIVE = ('ισι', 'ις')
+    ACCUSATIVE = 'νς'
+    VOCATIVE = 'ι'
